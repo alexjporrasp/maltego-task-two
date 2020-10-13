3893f89612cf9e97dcc61cf98b1337469b599fa6
Alex Porras <alexjporrasp@uma.es>
2020-10-13 14:19:39 +0200

    Add 'CertificateToIssuerName' transform.

--- transforms/CertificateToIssuerName.py
+++ transforms/CertificateToIssuerName.py
@@ -1,11 +1,19 @@
+from maltego_trx import entities
 from maltego_trx import transform
+import re
 
 
 class CertificateToIssuerName(transform.DiscoverableTransform):
     """
-    Returns the name (CN) of the certificate issuer as a maltego.Phrase entity.
+    Returns the name (CN) of the certificate issuer.
     """
 
     @classmethod
     def create_entities(cls, request, response):
-        raise NotImplementedError()
+        # Issuer Name Example:
+        # "C=US, O=Let's Encrypt, CN=Let's Encrypt Authority X3"
+        match = re.match(r".*CN=(.*)", request.getProperty('issuer_name'))
+        CN = request.getProperty('issuer_name')
+        if match:
+            CN = match.group(1)
+        response.addEntity(entities.Phrase, CN)
