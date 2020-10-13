341825c667526c75b4a9aa6c46c2930a2c647d34
Alex Porras <alexjporrasp@uma.es>
2020-10-13 14:53:30 +0200

    Change issuer name to include only the common name (CN)

--- transforms/CertificateToIssuerName.py
+++ transforms/CertificateToIssuerName.py
@@ -10,10 +10,4 @@ class CertificateToIssuerName(transform.DiscoverableTransform):
 
     @classmethod
     def create_entities(cls, request, response):
-        # Issuer Name Example:
-        # "C=US, O=Let's Encrypt, CN=Let's Encrypt Authority X3"
-        match = re.match(r".*CN=(.*)", request.getProperty('issuer_name'))
-        CN = request.getProperty('issuer_name')
-        if match:
-            CN = match.group(1)
-        response.addEntity(entities.Phrase, CN)
+        response.addEntity(entities.Phrase, request.getProperty('issuer_name'))
